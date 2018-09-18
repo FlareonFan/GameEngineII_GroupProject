44b9a87b0aa65d16189d3169500b9d9a808ab858
Andrew Letailleur <31889767+AndrewLetailleur@users.noreply.github.com>
2018-09-18 07:36:14 +0100

    Made a more efficient menu, and added a sketch on options

--- GameEngineIIProject/Assets/Code/MainMenuScript.cs
+++ GameEngineIIProject/Assets/Code/MainMenuScript.cs
@@ -5,47 +5,24 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;//for the Info switchin;
 
 public class MainMenuScript : MonoBehaviour {
-
-    public Text StoryTxt, InfoTxt, CreditsTxt;
-
-
-
-    // Use this for initialization
-    void Start() {
-        InfoTxt.enabled = false;
-        StoryTxt.enabled = false;
-        CreditsTxt.enabled = false;
-    }
     
-    public void PlayGame() {//play the first level, after menu. Scene wise
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+    /*No need for holding object variables, so their removed/[REDACTED]*/
 
-    public void LoadGame() { }//to load 'saved' save state
+    //play the first level, after menu. Scene wise
+    public void PlayGame() {SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);}
 
-    public void OptionsMenu() { }//todo, pop up somethinng
-    public void GoBack() { }//todo, pop up somethinng
-
-    public void Credits() {
-        CreditsTxt.enabled = true;
-        InfoTxt.enabled = false;
-        StoryTxt.enabled = false;
-    }//switch text, or load a sep. page
-
-    //begin switching between Lorem Ibsum
-    public void Info() {
-        InfoTxt.enabled = true;
-        StoryTxt.enabled = false;
-        CreditsTxt.enabled = false;
-    }
-    public void Story() {
-        StoryTxt.enabled = true;
-        InfoTxt.enabled = false;
-        CreditsTxt.enabled = false;
-    }
-
+    public void LoadGame() { }//to load 'saved' save state, scene database management wise
+    
     public void QuitGame() {
         Debug.Log("Game has closed");
         Application.Quit();
     }
+
+    /* No longer needed code, as GameObject.SetActive on OnClick() Unity does the trick just fine
+    public void Credits() {}
+    public void Info() {}
+    public void Story() {}
+    public void OptionsMenu() {}
+    public void GoBack() {}
+    */ /// end of no longer needed code
 }
